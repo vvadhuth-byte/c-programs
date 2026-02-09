7022eeba32272b52a1475939fde338b380b7b86f
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-09 13:49:08 +0530

    Add files via upload

---
+++ print two integers in one line.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a=5;
+    printf("%d ",a);
+    int b=10;
+    printf("%d\n",b);
+    int c=20;
+    printf("%d ",c);
+    int d=30;
+    printf("%d",d);
+
+    return 0;
+}
