cb5345e82a7474695933f4bc61922b6dd7e12b2d
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-12 12:02:33 +0530

    Add files via upload

---
+++ three digit number 2 .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    
+        if(num>100&&num<=999){
+        printf("three digit numbber");
+        }
+        else
+        printf("not a three digit nummbber");
+        
+        
+        
+
+    return 0;
+}
