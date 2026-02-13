c5986dcc236b4ccd3efb4974a9b8c9dd6294e061
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-13 10:24:15 +0530

    Add files via upload

---
+++ divisible by 11(2) .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%11==0){
+    printf("divisible by 11");
+    }
+    else
+    printf("not divisible by 11");
+    
+    
+    return 0;
+}
