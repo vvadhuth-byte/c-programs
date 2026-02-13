a79b25ef419c2e7059d8d34602eddb44ca29c8b5
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-13 12:04:23 +0530

    Add files via upload

---
+++ electricity bill .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d",&a);
+    if(a>=0&&a<=100){
+    printf("bill amount = Rs.%d",b);
+    }
+    else if(a>=101&&a<=500)
+    printf("bill amount = Rs.%d",c);
+    return 0;
+}
