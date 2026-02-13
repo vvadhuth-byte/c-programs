427217df94ce8b30dd192f9f1a0dc0052c3422b9
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-13 11:35:36 +0530

    Add files via upload

---
+++ number wiyh in range ( 2) .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>=10&&a<=20){
+    printf("withh in range");
+    }
+    else
+    printf("not in range");
+    return 0;
+}
