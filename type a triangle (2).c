d7b691a38a52f121921b2ff9ce5a330f0f40e5c2
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-13 10:31:04 +0530

    Add files via upload

---
+++ type a triangle (2).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a+b+c==180){
+    printf("valid triangle");
+    }
+    else
+    printf("in valid triangle");
+    
+    
+    return 0;
+}
