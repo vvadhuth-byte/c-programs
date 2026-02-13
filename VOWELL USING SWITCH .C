1621d7367cfd2d82a19fc4311796780d34efad79
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-13 12:20:40 +0530

    Add files via upload

---
+++ VOWELL USING SWITCH .C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf(" %c", &ch);  
+
+    switch(ch) {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            printf("Vowel");
+            break;
+        default:
+            printf("Consonant");
+    }
+
+    return 0;
+}
