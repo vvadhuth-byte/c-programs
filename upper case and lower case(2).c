9df2187198b2777e4b7ca87bfd0e03e7b9af5beb
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-13 10:21:16 +0530

    Add files via upload

---
+++ upper case and lower case(2).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char A;
+    scanf("%c",&A);
+    if(65<=A&&90>=A){
+    printf("upper case");
+    }
+    else
+    printf("lower case");
+    
+    
+    return 0;
+}
