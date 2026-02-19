56417e644d465f1f47c2bbeb28845dd8dd0c9edf
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-19 09:47:32 +0530

    Create for loop 19.c

---
+++ for loop 19.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    
+
+    scanf("%d%d", &a,&b);
+    
+        for(int i=1;i<=100;i++)
+        {
+            if (i%a==0 && i%b==0){
+            printf("%d",i);
+            break;
+            }
+           
+        }
+   
+    return 0;
+}
