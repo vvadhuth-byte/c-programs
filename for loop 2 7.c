4e63824b58417ec44e3eefebae51a01b0de90f87
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-21 12:00:20 +0530

    Create for loop 2 7.c

---
+++ for loop 2 7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a,i,e=0,sum=0;
+    scanf("%d",&a);
+    int b[
+    for(i=1;i<=a;i++){
+        scanf("%d",&b[i]);
+        sum+=b[i];
+        if(b[i]>50)AC
+        e++;
+    }
+    printf("average per day %d \n",sum);
+    printf("over rain=%d",e);
+
+    return 0;
+}
