96746c0439141873ca36cb87344af231119a8ddc
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-21 11:51:58 +0530

    Create for loop 2 4.c

---
+++ for loop 2 4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,i,sum=0;
+    scanf("%d%d",&a,&b);
+
+
+    for(i=1;i<=b;i++){
+        a=a-100;
+        
+    }
+    printf("final salary= %d",a);
+
+    return 0;
+}
