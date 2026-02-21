a30c09916da8ca92a58cf5dfeeb3d4f4a16c784f
vvadhuth-byte <vvadhuth@gmail.com>
2026-02-21 12:06:17 +0530

    Create for loop 2 9.c

---
+++ for loop 2 9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a,i,e=0,sum=0;
+    scanf("%d",&a);
+    int b[a];
+    for(i=1;i<=a;i++){
+        scanf("%d",&b[i]);
+        sum+=b[i];
+        if(b[i]<40)
+        e++;
+    }
+    printf("average mark= %d \n",sum/a);
+    printf("fail subjects=%d",e);
+
+    return 0;
+}
