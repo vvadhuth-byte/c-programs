99498107deffa548e626168509ce0434b5ebc579
vvadhuth-byte <vvadhuth@gmail.com>
2026-03-05 14:10:01 +0530

    Create array 4.c

---
+++ array 4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for (int i=0;i<a;i++) {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=a-1;i>=0;i--){
+        printf("%d ",arr[i]);
+    }
+   
+    return 0;
+}
