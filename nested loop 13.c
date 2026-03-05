e78273da22adb0b862e71cd698fb31197f641db5
vvadhuth-byte <vvadhuth@gmail.com>
2026-03-05 10:18:57 +0530

    Create nested loop 13.c

---
+++ nested loop 13.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5;
+
+    for(i = 1; i <= n; i++) {
+
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        for(j = 1; j <= 2*(n - i); j++) {
+            printf(" ");
+        }
+
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for(i = n - 1; i >= 1; i--) {
+
+        // Left Stars
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        for(j = 1; j <= 2*(n - i); j++) {
+            printf(" ");
+        }
+
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
