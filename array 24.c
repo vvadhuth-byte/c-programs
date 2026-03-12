433faedc596e63ba2a657ce1a88b0feea39057d2
vadhudullah <vvadhuth@gmail.com>
2026-03-12 13:27:34 +0530

    Create array 24.c

---
+++ array 24.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n; i++) {
+        for(j = i+1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                printf("No");
+                return 0;
+            }
+        }
+    }
+
+    printf("Yes");
+    return 0;
+}
