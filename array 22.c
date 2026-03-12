ec0cc54b48f6cbc2ff69056b64429ff4ece47ba6
vadhudullah <vvadhuth@gmail.com>
2026-03-12 13:25:41 +0530

    Create array 22.c

---
+++ array 22.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, count = 0;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 1; i < n-1; i++)
+        if(arr[i] > arr[i-1] && arr[i] > arr[i+1])
+            count++;
+
+    printf("%d", count);
+
+    return 0;
+}
