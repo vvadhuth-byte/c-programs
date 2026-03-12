8c0a88b27eb4fc1cc669add1be6865ee99fae10d
vadhudullah <vvadhuth@gmail.com>
2026-03-12 13:23:39 +0530

    Create array 20.c

---
+++ array 20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n; i++)
+        if(arr[i] < 0)
+            arr[i] = 0;
+
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
